4f82f40944a698bac841b91e9fea9d1b7f8bd3db
Abhay Marathe <abhaysm7@gmail.com>
2020-09-19 12:22:42 -0600

    Changed the CoderByte1 program to run properly

--- hackerrank/CoderByte1.java
+++ hackerrank/CoderByte1.java
@@ -11,44 +11,51 @@ import java.io.*;
  * @author abhay
  */
 public class CoderByte1 {
+    public static void main(String []args){
+        String[]  s  = {"B:-1","A:1","B:3","A:5"};
+        String[]  s1 = {"X:-1","Y:1","X:-4","B:3","X:5"};
+        String[]  s2 = {"Z:0","A:-1"};
+        
+        System.out.println(SearchingChallenge(s));
+        System.out.println(SearchingChallenge(s1));
+        System.out.println(SearchingChallenge(s2));
+        
+     }
+   
     public static String SearchingChallenge(String[] strArr){
        HashMap<String, Integer> map = new HashMap<>();
        
-       for(int i = 0; i < strArr.length; i++){
+        for(int i = 0; i < strArr.length; i++){
+           
            String namepass[] = strArr[i].split(":"); 
            String key = namepass[0]; 
            String val = namepass[1];
-           
+      
            if(!map.containsKey(key)){
                map.put(key,Integer.valueOf(val));
+               
            }else{
-               map.put(key, map.get(key) + Integer.valueOf(val));
+               map.put(key, map.get(key) + Integer.valueOf(val));  
            }
-       }
+        }
        
-       ArrayList<String> sortedKeys = new ArrayList<String>(map.keySet()); 
+       ArrayList<String> sortedKeys = new ArrayList<>(map.keySet()); 
        Collections.sort(sortedKeys);
        
+       String result = " ";
        
-       String result = "";
-       int flag = 0;
+       int flag = sortedKeys.size();
+       
        for (String x : sortedKeys){
+           
            if(map.get(x) != 0){
-               result = x + ":" + map.get(x);
+               result = result + x + ":" + map.get(x);
            }
-           if(flag == 0){
-               result = result;
-           }else{
+           if(flag > 1 && map.get(x) != 0){
                result = result + ",";
            }
-           flag = 1;
+           flag = flag - 1;
        }  
-  
-        return result;
+       return result;
     }
-    
-     public static void main(String []args){
-        Scanner s = new Scanner(System.in);
-        System.out.print(SearchingChallenge(s.nextLine()));
-     }
 }
